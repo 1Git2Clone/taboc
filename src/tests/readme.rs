57ce0db8d888d0b546a234e542a3e625a1b884ea
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2025-02-02 22:11:12 +0200

    Update test

--- src/tests/readme.rs
+++ src/tests/readme.rs
@@ -2,23 +2,23 @@ use crate::prelude::*;
 
 #[test]
 fn test_mock_input_file() -> Result<(), Error> {
-    let expected = "## Table of contents
+    let expected = "
+
+## Table of contents
 
 - [Test](#test)
   - [Heading Two](#heading-two)
     - [Heading Three](#heading-three)
       - [Heading Four](#heading-four)
         - [Heading Five](#heading-five)
-  - [Heading Two #2](#heading-two-#2)
-    - [Heading Three #2](#heading-three-#2)
-    - [Heading Three #3](#heading-three-#3)
-      - [Heading Four #2](#heading-four-#2)
-        - [Heading Five #2](#heading-five-#2)
-        - [Heading Five #3](#heading-five-#3)
-      - [Heading Four #3](#heading-four-#3)
-  - [Heading Two #3](#heading-two-#3)
-
-";
+  - [Heading Two Number 2](#heading-two-number-2)
+    - [Heading Three Number 2](#heading-three-number-2)
+    - [Heading Three Number 3](#heading-three-number-3)
+      - [Heading Four Number 2](#heading-four-number-2)
+        - [Heading Five Number 2](#heading-five-number-2)
+        - [Heading Five Number 3](#heading-five-number-3)
+      - [Heading Four Number 3](#heading-four-number-3)
+  - [Heading Two Number 3](#heading-two-number-3)";
 
     let file = std::fs::File::open(std::env::current_dir()?.join("mock_data/README.md"))?;
 
